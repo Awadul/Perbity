20a6c165b70f28f534e20ce3357881f2a321680a
Muhammad Awais Abdullah <awaisabdullahm@gmail.com>
2025-11-16 00:07:05 +0500

    Fix case-sensitive import for SignUp component and remove demo credentials

--- Ajmal/src/App.jsx
+++ Ajmal/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import ProtectedRoute from './components/ProtectedRoute.jsx'
 import Login from './pages/Login.jsx'
-import SignUp from './pages/Signup.jsx'
+import SignUp from './pages/SignUp.jsx'
 import Dashboard from './pages/Dashboard.jsx'
 import AdminDashboard from './pages/AdminDashboard.jsx'
 import ViewAds from './pages/ViewAds.jsx'
